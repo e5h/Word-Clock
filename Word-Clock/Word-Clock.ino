928d2186c86185badd3e052bc556be7e3cede989
e5h <32693617+e5h@users.noreply.github.com>
2017-10-15 13:38:27 -0600

    added template file

---
+++ Word-Clock/Word-Clock.ino
@@ -0,0 +1,24 @@
+#include <Adafruit_NeoPixel.h>
+/* -------------------9-
+ * --n---n---n---n---n--
+ *  I T U I S H A P P Y
+ *  T W E N T Y H A L F
+ *  K B D A Y H F I V E
+ *  T E N Q U A R T E R
+ *  T O P A S T P A P A
+ *  E T E N S E V E N T
+ *  I Y F I V E S I X H
+ *  G R A N N Y F O U R
+ *  H E L E V E N O N E
+ *  T W E L V E T W O E
+ * ----u---u---u---u----
+ * 99-----------------0-
+ */
+
+void setup() {
+
+}
+
+void loop() {
+
+}
